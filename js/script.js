5753172027f378d7d55edc1163bd592959d9dd30
reyz1337 <andrlav65@gmail.com>
2022-09-21 00:04:40 +0300

    add task in code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', () =>{
 //minimum
-//1
+//1 Напиши всі можливі варіанти створення функцій.
     /* function fuction1() { };
     let fuction2 = function () { };
     let function3 = () => { }; */
 
-//2
+//2 Створи функцію, яка буде виводити кількість переданих їй аргументів.
     function countArg(){
         alert(`Кількість переданих аргументів: ${arguments.length}`);
     }
     countArg(5,7,10,'p');
 
-//3
+//3 Напиши функцію, яка приймає 2 числа і повертає :
+    // 1. -1, якщо перше число менше, ніж друге; 
+    // 2. 1 - якщо перше число більше, ніж друге; 
+    // 3. 0 - якщо числа рівні.
     function ret(a, b){
         if(a < b){
             return -1;
@@ -28,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
     alert(ret(5,7));
     
-//4
+//4 Напиши функцію, яка обчислює факторіал переданого їй числа.
     function factorial(n){
         let fact = 1;
         if (n == 0 || n == 1){
@@ -43,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     let n = 5;
     alert(`Факторіал числа ${n} = ${factorial(n)}`);
 
-//5
+//5 Напиши функцію, яка приймає три окремі цифри і перетворює їх в одне число. Наприклад: цифри 1, 4, 9 перетворяться в число 149.
     function str(){
         let a = '2';
         let b = '3';
@@ -53,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
     str();
 
-//6
+//6 Напиши функцію, яка приймає довжину і ширину прямокутника і обчислює його площу. Якщо в функцію передали 1 параметр, то вона обчислює площу квадрата.
     function square(a, b){
         if((typeof b) == 'undefined'){
             return a*a;
@@ -65,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     alert(square(5));
 
 //normal
-//1
+//1 Напиши функцію, яка перевіряє, чи є передане їй число “досконалим числом”. Досконале число - це число, яке дорівнює сумі всіх своїх дільників.
     function perfect_number(num){
         if (isNaN(num)) {
             return 'введіть число';
@@ -90,8 +93,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     alert(perfect_number(28));
     alert(perfect_number(1));
 
-//2
-
+//2 Напиши функцію, яка приймає мінімальне і максимальне значення для діапазону, і виводить тільки ті числа з діапазону, які є досконалими. Використовуй написану раніше функцію, щоб дізнатися, чи є це число досконалим.
     function perfect_num(num){
         if (isNaN(num)) {
             return 'введіть число';
